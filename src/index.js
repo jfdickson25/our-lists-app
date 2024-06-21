a3da77f57f9b7ec6e51a7994a9c90336a9282ab3
Jordan Dickson <jfdickson25@gmail.com>
2024-06-20 20:49:47 -0600

    Set max width to 600px

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div id="application">
+      <App />
+    </div>
   </React.StrictMode>
 );
 
